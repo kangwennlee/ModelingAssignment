fae41a23c7cb3ca27b849fd56d02e52a34dc1ce8
Lee Kang Wenn <kangwenn@gmail.com>
2017-12-28 22:41:44 +0800

    Created point estimate.java

---
+++ src/modelingassignment/PointEstimate.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelingassignment;
+
+/**
+ *
+ * @author Kangwenn
+ */
+public class PointEstimate {
+    static double[] averageWaitingTime={0,0,0,0,0};
+    static double[] averageServiceTime={0,0,0,0,0};
+    static double[] averageInterArrivalTime={0,0,0,0,0};
+    
+    public static void calculatePointEstimate(){
+        
+    }
+}
