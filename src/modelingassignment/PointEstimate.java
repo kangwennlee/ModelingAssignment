706c3b3f6a7259d0141eb08406b85acc7ff66e37
Lee Kang Wenn <kangwenn@gmail.com>
2017-12-29 18:23:37 +0800

    630PM push

--- src/modelingassignment/PointEstimate.java
+++ src/modelingassignment/PointEstimate.java
@@ -12,18 +12,36 @@ package modelingassignment;
 public class PointEstimate {
 
     public static void main(String[] args) {
+        firstScenario();
+        secondScenario();
+        thirdScenario();
+    }
+    
+    public static void firstScenario(){
         //First scenario
-        double[] averageWaitingTime = {0.02, 0, 0, 0.005, 0.0025};
-        double[] averageServiceTime = {2.3125, 2.2575, 2.3, 1.9825, 2.0725};
-        double[] averageInterArrivalTime = {0.9, 0.8975, 0.8975, 0.9, 0.895};
+        double[] averageWaitingTime = {0.02046035806, 0, 0, 0.005540166205,0.002577319588};
+        double[] averageServiceTime = {2.3657289,2.274559194,2.371134021,2.1966759,2.136597938};
+        double[] averageInterArrivalTime = {0.9207161125,0.9042821159,0.925257732,0.9972299169,0.9226804124};
+        calculatePointEstimate(averageWaitingTime);
+        calculatePointEstimate(averageServiceTime);
+        calculatePointEstimate(averageInterArrivalTime);
+    }
+    
+    public static void secondScenario(){
         //Second scenario
-        //double[] averageWaitingTime={0.0025,0,0,0.005,0.0025};
-        //double[] averageServiceTime={2.0075,2.0325,2.0575,1.775,1.7275};
-        //double[] averageInterArrivalTime={0.9,0.8975,0.8975,0.9,0.895};
+        double[] averageWaitingTime={0.002557544757,0,0,0.005540166205,0.002577319588};
+        double[] averageServiceTime={2.05370844,2.047858942,2.121134021,1.969529086,1.780927835};
+        double[] averageInterArrivalTime={0.9207161125,0.9042821159,0.925257732,0.9972299169,0.9226804124};
+        calculatePointEstimate(averageWaitingTime);
+        calculatePointEstimate(averageServiceTime);
+        calculatePointEstimate(averageInterArrivalTime);
+    }
+    
+    public static void thirdScenario(){
         //Third scenario
-        //double[] averageWaitingTime={0.07,0,0.0275,0.0025,0.0025};
-        //double[] averageServiceTime={2.315,2.2575,2.3,1.9825,2.0725};
-        //double[] averageInterArrivalTime={0.9,0.8975,0.8975,0.9,0.895};
+        double[] averageWaitingTime={0.0716112532,0,0.01288659794,0.002770083102,0.002577319588};
+        double[] averageServiceTime={2.3657289,2.274559194,2.371134021,2.1966759,2.136597938};
+        double[] averageInterArrivalTime={0.9207161125,0.9042821159,0.925257732,0.9972299169,0.9226804124};
         calculatePointEstimate(averageWaitingTime);
         calculatePointEstimate(averageServiceTime);
         calculatePointEstimate(averageInterArrivalTime);
